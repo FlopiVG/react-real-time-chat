5394984f8951b6cf4a873001296dd3f2f622bc94
FlopiVG <flopivillagarcia@gmail.com>
2017-05-30 21:00:23 +0200

    react-chat

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const socketServer = require('./socket-server');
 const PORT = process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 const webServer = app.listen(PORT, () => {
